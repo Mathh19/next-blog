be88d18e20bee10c6dec9a0e6b711418aa77c8a6
Matheus <mf635097@gmail.com>
2022-10-03 08:51:55 -0300

    Feito paginas de autores, tags e categorias

---
+++ src/pages/tag/[slug].tsx
@@ -0,0 +1,63 @@
+import { loadPosts, StrapiPostAndSettings } from 'api/load-posts';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { PostsTemplate } from 'templates/PostsTemplate';
+
+export default function AuthorPage({ posts, setting }: StrapiPostAndSettings) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
+  const tagName = posts.data[0].attributes.tags.data.filter(
+    (tag) => tag.attributes.slug === router.query.slug,
+  )[0].attributes.name;
+
+  return (
+    <>
+      <Head>
+        <title>
+          Tag: {tagName} - {setting.data.attributes.blogName}
+        </title>
+      </Head>
+      <PostsTemplate posts={posts} settings={setting} />
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<StrapiPostAndSettings> = async (
+  ctx,
+) => {
+  let data = null;
+
+  try {
+    data = await loadPosts({
+      tagSlug: { contains: ctx.params.slug as string },
+    });
+  } catch (err) {
+    data = null;
+  }
+
+  if (!data || !data.posts || !data.posts.data.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+    },
+    revalidate: 60,
+  };
+};
